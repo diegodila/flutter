417f5ff4ad8369cce7576141459a49a3a104792d
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-10-18 22:34:29 -0300

    adding models

--- proofs/am/am_flutter_final/lib/models/municipio_model.dart
+++ proofs/am/am_flutter_final/lib/models/municipio_model.dart
@@ -1,11 +1,34 @@
+import 'dart:convert';
+
 class MunicipioModel {
   int id;
   String nome;
   String link;
+  String url;
 
   MunicipioModel({
     this.id,
     this.nome,
     this.link,
+    this.url,
   });
+
+  factory MunicipioModel.fromJson(String str) =>
+      MunicipioModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory MunicipioModel.fromMap(Map<String, dynamic> json) => MunicipioModel(
+        id: (json["id"] is int) ? json["id"] : int.parse(json["id"]),
+        nome: json["nome"],
+        link: json["link"],
+        url: json["url"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "nome": nome,
+        "link": link,
+        "url": url,
+      };
 }
